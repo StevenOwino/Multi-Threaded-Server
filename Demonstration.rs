f0ef3f0bd82daf86a9c82036b5ba5ceda0189907
Steve Owino <90975591+StevenOwino@users.noreply.github.com>
2021-11-09 19:08:39 +0300

    Add files via upload
    Put these in root folder "hello" : Demonstration.rs, 404.html, and hello.html.
    Create a new folder under hello: name it "src".  Copy  the "lib.rs" file here.
    Under the folder "src", create another folder and name it "bin". Copy the "main.rs" file.

---
+++ Demonstration.rs
@@ -0,0 +1,48 @@
+Remember to terminate the server with Control C.
+
+Check that the code complies without any warnings: 
+ $ cargo check
+
+ (base) Steven-Owinos-iMac:hello stevenowino$ cargo check
+    Checking hello v0.1.0 (/Users/stevenowino/RUST_Cookbook/hello)
+    Finished dev [unoptimized + debuginfo] target(s) in 4.14s
+(base) Steven-Owinos-iMac:hello stevenowino$
+
+Change the code on line 26 on the "main.rs" file to: 
+ Line 26 : for stream in listener.incoming().take(2) { 
+
+   (base) Steven-Owinos-iMac:hello stevenowino$ cargo check
+    Finished dev [unoptimized + debuginfo] target(s) in 0.01s
+
+Start the server with : 
+ $ cargo run 'hello' 
+   
+   (base) Steven-Owinos-iMac:hello stevenowino$ cargo run 'hello'
+      Compiling hello v0.1.0 (/Users/stevenowino/RUST_Cookbook/hello)
+       Finished dev [unoptimized + debuginfo] target(s) in 2.45s
+        Running `target/debug/main hello`
+
+Then, make requests on your web browser,
+To demonstrate that the graceful shutdown and cleanup is in working order.
+http://127.0.0.1/ and http://127.0.0.1/sleep on multiple tabs 
+
+
+Shutting down.
+Sending terminate message to all workers.
+Shutting down all workers.
+Shutting down worker 0
+Worker 0 got a job; executing.
+Worker 1 got a job; executing.
+Worker 2 was told to terminate.
+Worker 3 was told to terminate.
+Worker 0 was told to terminate.
+Shutting down worker 1
+Worker 1 was told to terminate.
+Shutting down worker 2
+Shutting down worker 3
+(base) Steven-Owinos-iMac:hello stevenowino$
+
+
+
+
+
---
+++ main.rs
@@ -0,0 +1,94 @@
+// This code will listen  for an HTTP request at the address 127.0.0.1:3571 for incoming
+// Tcp streams, respond, and render the HTML source file, ASYNCHRONOULSLY.
+// The "take" method can be defined in the Iterator trait to limit the iteration
+// to the first two items at most. 
+// The ThreadPool will go out of scope at the end of main,
+// and the "drop" implementation will run. 
+
+use hello::ThreadPool;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::fs;
+use std::thread;
+use std::time::Duration;
+
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:3571").unwrap();
+    // Ports 0 to 1023 are Well-Known Ports.
+    // Ports 1024 to 49151 are Registered Ports (often registered by a software developer 
+    // to designate a particular port for their application)
+    // Ports 49152 to 65535 are Public Ports.
+
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {  
+    // insert .take(2) before the opening curly brace above
+    // To demonstrate graceful shutdown and cleanup
+        let stream = stream.unwrap();
+        
+        pool.execute(|| {   
+        //thread::spawn(|| {
+            handle_connection(stream);
+        });
+
+    }
+
+    println!("Shutting down.");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b" /sleep HTTP/1.1\r\n";
+
+
+    //if buffer.starts_with(get) {
+
+    //  let contents = fs::read_to_string("hello.html").unwrap();
+
+    //  let response = format!( 
+    //      "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+    //      contents.len(),
+    //      contents
+    //);
+
+    //stream.write(response.as_bytes()).unwrap();
+    //stream.flush().unwrap();
+
+  //}
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!( 
+          "{}\r\nContent-Length: {}\r\n\r\n{}",
+          status_line,
+          contents.len(),
+          contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+
+    
+  }
+
+    //println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+// Remember to stop the program by pressing ctrl-c 
+// when you’re done running a particular version of the code. 
+// Then restart: cargo run 'hello' after you’ve made each set of code changes
+// to make sure you’re running the newest code.
